fa7bb514098d9687a833baf3dda1649e0dbcd909
DavidoffNeal <DavidoffNeal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-09 08:37:12 +0000

    Initial release of Simple Share Buttons Adder!! Version 1.0 2013-03-09
    git-svn-id: https://plugins.svn.wordpress.org/simple-share-buttons-adder/trunk@678606 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,43 @@
+﻿=== Simple Share Buttons Adder ===
+Contributors: DavidoffNeal
+Donate link: http://www.davidsneal.co.uk/contributions
+Tags: share buttons, facebook, twitter, google+, share, share links
+Requires at least: 3.0.1
+Tested up to: 3.5.1
+Stable tag: 1.0
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A simple plugin that enables you to add share buttons to all of your posts and/or pages.
+
+== Description ==
+
+The Simple Share Buttons Adder (SSBA) does exactly what it says on the tin – adds share buttons to all of your posts and pages, simply.
+
+No hotlinking to another website to slow downloading or for tracking use, it's simple, as promised.
+
+SSBA utilises features that, as a WordPress user, you will be familiar with. A self-explanatory administration screen will have you showing your Share Buttons on your posts and pages in no time!
+
+== Installation ==
+
+1. Upload the 'simple-share-buttons-adder' folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Navigate to the SSBA Admin Panel via ‘Plugins’ -> ‘Share Buttons’ to change the settings as desired.
+
+== Frequently Asked Questions ==
+
+SSBA has only just been released so I've not had any questions yet! I'll be happy to answer any that come up!
+
+== Screenshots ==
+
+1. This is the SSBA Admin menu, where you can change the appearance and placement of your Share Buttons.
+2. This is an example of a set of Share Buttons you can add using Simple Share Buttons Adder.
+3. This is an example of a set of Share Buttons you can add using Simple Share Buttons Adder.
+4. This is an example of a set of Share Buttons you can add using Simple Share Buttons Adder.
+5. This is an example of a set of Share Buttons you can add using Simple Share Buttons Adder.
+6. This is an example of a set of Share Buttons you can add using Simple Share Buttons Adder.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
---
+++ simple-share-buttons-adder.php
@@ -0,0 +1,342 @@
+<?php
+/*
+Plugin Name: Simple Share Buttons Adder
+Plugin URI: http://www.davidsneal.co.uk/wordpress/simple-share-buttons-adder
+Description: A simple plugin that enables you to add share buttons to all of your posts and/or pages.
+Version: 1.0
+Author: David S. Neal
+Author URI: http://www.davidsneal.co.uk/
+License: GPLv2
+
+Copyright 2013 David S Neal me@davidsneal.co.uk
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as 
+published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+	// --------- INSTALLATION ------------ //
+
+	// run the activation function upon acitvation of the plugin
+	register_activation_hook( __FILE__, 'ssba_activate' );
+	
+	// activate ssba function
+	function ssba_activate() {
+		
+		// insert default options for ssba
+		add_option('ssba_version', 			'1.0');
+		add_option('ssba_image_set', 		'default');
+		add_option('ssba_posts_or_pages',	'both');
+		add_option('ssba_align', 			'left');
+		add_option('ssba_padding', 			'10');
+		add_option('ssba_before_or_after', 	'after');
+		add_option('ssba_google', 			'Y');
+		add_option('ssba_facebook', 		'Y');
+		add_option('ssba_twitter', 			'Y');
+		add_option('ssba_diggit', 			'Y');
+	}
+
+	// --------- ADMIN BITS ------------ //
+	
+	// add menu to dashboard
+	add_action( 'admin_menu', 'ssba_menu' );
+
+	// menu settings
+	function ssba_menu() {
+	
+		// add menu page
+		add_plugins_page( 'Simple Share Buttons Adder', 'Share Buttons', 'manage_options', 'simple-share-buttons-adder', 'ssba_settings');
+	}
+
+	// --------- SETTINGS PAGE ------------ //
+
+	// answer form
+	function ssba_settings() {
+	
+		//' check if user has the rights to manage options
+		if ( !current_user_can( 'manage_options' ) )  {
+		
+			// permissions message
+			wp_die( __('You do not have sufficient permissions to access this page.'));
+		}
+	
+		// check for submitted form
+		if (isset($_POST['ssba_options'])) {
+
+			// update existing ssba settings		
+			update_option('ssba_image_set', 		($_POST['ssba_image_set']));
+			update_option('ssba_posts_or_pages', 	($_POST['ssba_posts_or_pages']));
+			update_option('ssba_align', 			($_POST['ssba_align']));
+			update_option('ssba_padding', 			($_POST['ssba_padding']));								
+			update_option('ssba_before_or_after', 	($_POST['ssba_before_or_after']));				
+			update_option('ssba_google', 			($_POST['ssba_google']));
+			update_option('ssba_facebook', 			($_POST['ssba_facebook']));
+			update_option('ssba_twitter', 			($_POST['ssba_twitter']));
+			update_option('ssba_diggit', 			($_POST['ssba_diggit']));
+
+			// show settings saved message
+			echo '<div id="setting-error-settings_updated" class="updated settings-error"><p><strong>Settings saved.</strong></p></div>';
+		}
+
+		// query the db for current ssba settings
+		$arrSettings = get_ssba_settings();
+
+		// --------- HTML FORM ------------ //
+		
+		// html form
+		echo '<div class="wrap">';
+			echo '<img src="' . plugins_url( 'images/ssba.png' , __FILE__ ) . '" align="left" style="margin-right: 10px;" alt="ssba" /> ';
+			echo '<h2>Simple Share Buttons Adder</h2>';
+			echo '<form method="post">';
+			
+			// hidden field to check for post
+			echo '<input type="hidden" name="ssba_options" id="ssba_options" value="save" />';
+			
+				// continue outputting the form
+				echo '<table class="form-table">';
+					echo '<tr valign="top">';
+						echo '<th scope="row" style="width: 120px;"><label for="ssba_posts_or_pages">Location:&nbsp;</label></th>';
+						echo '<td><select name="ssba_posts_or_pages" id="ssba_posts_or_pages">';
+						echo '<option ' . ($arrSettings['ssba_posts_or_pages'] == 'both'  ? 'selected="selected"' : NULL) . ' value="both">Both</option>';
+						echo '<option ' . ($arrSettings['ssba_posts_or_pages'] == 'posts' ? 'selected="selected"' : NULL) . ' value="posts">Posts</option>';
+						echo '<option ' . ($arrSettings['ssba_posts_or_pages'] == 'pages' ? 'selected="selected"' : NULL) . ' value="pages">Pages</option>';
+						echo '</td>';
+					echo '</tr>';
+					echo '<tr valign="top">';
+						echo '<th scope="row" style="width: 120px;"><label for="ssba_before_or_after">Placement:&nbsp;</label></th>';
+						echo '<td><select name="ssba_before_or_after" id="ssba_before_or_after">';
+						echo '<option ' . ($arrSettings['ssba_before_or_after'] == 'after' 	? 'selected="selected"' : NULL) . ' value="after">After</option>';
+						echo '<option ' . ($arrSettings['ssba_before_or_after'] == 'before' ? 'selected="selected"' : NULL) . ' value="before">Before</option>';
+						echo '<option ' . ($arrSettings['ssba_before_or_after'] == 'both' 	? 'selected="selected"' : NULL) . ' value="both">Both</option>';
+						echo '</select></td>';
+					echo '</tr>';
+					echo '<tr valign="top">';
+						echo '<th scope="row" style="width: 120px;"><label for="ssba_align">Alignment:&nbsp;</label></th>';
+						echo '<td><select name="ssba_align" id="ssba_align">';
+						echo '<option ' . ($arrSettings['ssba_align'] == 'left'   ? 'selected="selected"' : NULL) . ' value="left">Left</option>';
+						echo '<option ' . ($arrSettings['ssba_align'] == 'center' ? 'selected="selected"' : NULL) . ' value="center">Center</option>';
+						echo '</select></td>';
+					echo '</tr>';
+					echo '<tr valign="top">';
+						echo '<th scope="row" style="width: 120px;"><label for="ssba_padding">Padding:&nbsp;</label></th>';
+						echo '<td><input type="number" name="ssba_padding" id="ssba_padding" step="1" min="0" value="' . $arrSettings['ssba_padding'] . '" /><span class="description">px</span>';
+						echo '</td>';
+					echo '</tr>';
+					echo '<tr valign="top">';
+						echo '<th scope="row" style="width: 120px;"><label for="ssba_image_set">Image Set:&nbsp;</label></th>';
+						echo '<td><select name="ssba_image_set" id="ssba_image_set">';
+						echo '<option ' . ($arrSettings['ssba_image_set'] == 'default' 		? 'selected="selected"' : NULL) . ' value="default">Default</option>';
+						echo '<option ' . ($arrSettings['ssba_image_set'] == 'arbenta' 		? 'selected="selected"' : NULL) . ' value="arbenta">Arbenta</option>';
+						echo '<option ' . ($arrSettings['ssba_image_set'] == 'pagepeel' 	? 'selected="selected"' : NULL) . ' value="pagepeel">Page Peel</option>';
+						echo '<option ' . ($arrSettings['ssba_image_set'] == 'plain' 		? 'selected="selected"' : NULL) . ' value="plain">Plain</option>';
+						echo '<option ' . ($arrSettings['ssba_image_set'] == 'ribbons' 		? 'selected="selected"' : NULL) . ' value="ribbons">Ribbons</option>';
+						echo '</select></td>';
+					echo '</tr>';
+					echo '<tr valign="top">';
+						echo '<th scope="row" style="width: 120px;"><label for="ssba_choices">Include:</label></th>';
+						echo '<td>';
+						echo 'Facebook&nbsp;<input type="checkbox" name="ssba_facebook" id="ssba_facebook" ' 	. ($arrSettings['ssba_facebook'] 	== 'Y'   ? 'checked' : NULL) . ' value="Y" style="margin-right: 10px;" />';
+						echo 'Twitter&nbsp;<input type="checkbox" name="ssba_twitter" id="ssba_twitter" ' 	. ($arrSettings['ssba_twitter'] 	== 'Y'   ? 'checked' : NULL) . ' value="Y" style="margin-right: 10px;" />';
+						echo 'Google&nbsp;<input type="checkbox" name="ssba_google" id="ssba_google" ' 		. ($arrSettings['ssba_google'] 		== 'Y'   ? 'checked' : NULL) . ' value="Y" style="margin-right: 10px;" />';
+						echo 'Digg&nbsp;<input type="checkbox" name="ssba_diggit" id="ssba_diggit" ' 			. ($arrSettings['ssba_diggit'] 		== 'Y'   ? 'checked' : NULL) . ' value="Y" style="margin-right: 10px;" />';
+						echo '</td>';
+					echo '</tr>';
+					echo '<tr valign="top">';
+						echo '<td><input type="submit" value="Save changes" id="submit" class="button button-primary"/></td>';
+					echo '</tr>';
+				echo '</table>';
+			echo '</form>';
+			
+			// image set preview below
+			echo '<br />';
+			echo '<h2>Image Sets</h2>';
+			echo '<div id="ssba_preview">';
+			echo '<h3>Default</h3>';
+			echo '<img src="' . WP_PLUGIN_URL . '/simple-share-buttons-adder/images/default.png" style="padding: 5px" />';
+			echo '<h3>Arbenta</h3>';
+			echo '<img src="' . WP_PLUGIN_URL . '/simple-share-buttons-adder/images/arbenta.png" style="padding: 5px" />';
+			echo '<h3>Page Peel</h3>';
+			echo '<img src="' . WP_PLUGIN_URL . '/simple-share-buttons-adder/images/pagepeel.png" style="padding: 5px" />';
+			echo '<h3>Plain</h3>';
+			echo '<img src="' . WP_PLUGIN_URL . '/simple-share-buttons-adder/images/plain.png" style="padding: 5px" />';
+			echo '<h3>Ribbons</h3>';
+			echo '<img src="' . WP_PLUGIN_URL . '/simple-share-buttons-adder/images/ribbons.png" style="padding: 5px" />';
+			echo '</div>';
+			echo '<p class="description">I hope you find this plugin useful. Should you come across any problems or have any suggestions, please <a href="http://www.davidsneal.co.uk/wordpress/simple-share-buttons-adder" target="_blank">leave a comment on this page</a>.</p>';
+			
+		// close #wrap	
+		echo '</div>';
+	}
+	
+	// --------- SHARE BUTTONS ------------ //
+	
+	// return ssba settings
+	function get_ssba_settings() {
+	
+		// globals
+		global $wpdb;
+		
+		// query the db for current ssba settings
+		$arrSettings = $wpdb->get_results("SELECT option_name, option_value
+											 FROM $wpdb->options 
+											WHERE option_name LIKE 'ssba_%'");
+											
+		// loop through each setting in the array
+		foreach ($arrSettings as $setting) {
+		
+			// add each setting to the array by name
+			$arrSettings[$setting->option_name] =  $setting->option_value;
+		}
+	
+		// return
+		return $arrSettings;	
+	}
+	
+	// get set share buttons
+	function get_share_buttons($arrSettings) {
+	
+		// variables
+		$htmlShareButtons = '';
+		$urlCurrentPage = get_current_url();
+		
+		// check for facebook
+		if ($arrSettings['ssba_facebook'] == Y) {
+		
+			// show facebook share button
+			$htmlShareButtons .= '<a href="http://www.facebook.com/sharer.php?u=' . $urlCurrentPage  . '">';
+			$htmlShareButtons .= '<img src="' . WP_PLUGIN_URL . '/simple-share-buttons-adder/buttons/' . $arrSettings['ssba_image_set'] . '/facebook.png" style="padding: ' . $arrSettings['ssba_padding'] . 'px" /></a>';
+		}
+		
+		// check for facebook
+		if ($arrSettings['ssba_twitter'] == Y) {
+		
+			// show facebook share button
+			$htmlShareButtons .= '<a href="http://twitter.com/share?url=' . $urlCurrentPage  . '">';
+			$htmlShareButtons .= '<img src="' . WP_PLUGIN_URL . '/simple-share-buttons-adder/buttons/' . $arrSettings['ssba_image_set'] . '/twitter.png" style="padding: ' . $arrSettings['ssba_padding'] . 'px" /></a>';
+		}
+		
+		// check for facebook
+		if ($arrSettings['ssba_google'] == Y) {
+		
+			// show facebook share button
+			$htmlShareButtons .= '<a href="https://plus.google.com/share?url=' . $urlCurrentPage  . '">';
+			$htmlShareButtons .= '<img src="' . WP_PLUGIN_URL . '/simple-share-buttons-adder/buttons/' . $arrSettings['ssba_image_set'] . '/google.png" style="padding: ' . $arrSettings['ssba_padding'] . 'px" /></a>';
+		}
+		
+		// check for facebook
+		if ($arrSettings['ssba_diggit'] == Y) {
+		
+			// show facebook share button
+			$htmlShareButtons .= '<a href="http://www.digg.com/submit?url=' . $urlCurrentPage  . '">';
+			$htmlShareButtons .= '<img src="' . WP_PLUGIN_URL . '/simple-share-buttons-adder/buttons/' . $arrSettings['ssba_image_set'] . '/diggit.png" style="padding: ' . $arrSettings['ssba_padding'] . 'px" /></a>';
+		}
+		
+		// return share buttons
+		return $htmlShareButtons;
+	}
+	
+	// get the current url
+	function get_current_url () {
+	
+		// variables
+		 $pageURL = 'http';
+		 
+		// add s to the url if required
+		($_SERVER['HTTPS'] == 'on' ? $pageURL .= 's' : NULL);
+		 
+		// add colon and slashes
+		$pageURL .= '://';
+		 
+		// check if server is not equal to 80
+		if ($_SERVER['SERVER_PORT'] != '80') {
+		
+			// add the port if needed
+			$pageURL .= $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . $_SERVER['REQUEST_URI'];
+		} else {
+		
+			// add url details without port
+			$pageURL .= $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+		}
+		
+		// return the url
+		return $pageURL;
+	}
+	
+	// get and show share buttons
+	function show_share_buttons($content) {
+	
+		// globals
+		global $post;
+		
+		// variables
+		$htmlContent = $content;
+		$htmlShareButtons = '';
+		$strIsWhatFunction = '';
+	
+		// get sbba settings
+		$arrSettings = get_ssba_settings();
+		
+		// switch for posts or pages
+		switch ($arrSettings['ssba_posts_or_pages']) {
+		
+			case 'both': // show on posts and pages
+			$strIsWhatFunction = is_single() || is_page();
+			break;
+			
+			case 'posts': // posts only
+			$strIsWhatFunction = is_single();
+			break;
+			
+			case 'pages': // pages only
+			$strIsWhatFunction = is_page();
+			break;
+		}
+
+		// placement on pages/posts/categories
+		if ($strIsWhatFunction) {
+		
+			// css style
+			$htmlShareButtons = '<style type="text/css">
+									#ssba 	{ 	
+												padding: 		  ' . $arrSettings['ssba_padding'] . 'px;
+											}
+								</style>';
+			
+			// content
+			$htmlShareButtons.= '<div id="ssba">';
+			$htmlShareButtons.= ($arrSettings['ssba_align'] == 'center' ? '<center>' : NULL);
+			$htmlShareButtons.= get_share_buttons($arrSettings);
+			$htmlShareButtons.= ($arrSettings['ssba_align'] == 'center' ? '</center>' : NULL);
+			$htmlShareButtons.= '</div>';
+			
+			// switch for placement of ssba
+			switch ($arrSettings['ssba_before_or_after']) {
+			
+				case 'before': // before the content
+				$htmlContent = $htmlShareButtons . $content;
+				break;
+				
+				case 'after': // after the content
+				$htmlContent = $content . $htmlShareButtons;
+				break;
+				
+				case 'both': // before and after the content
+				$htmlContent = $htmlShareButtons . $content . $htmlShareButtons;
+				break;
+			}
+		}
+		
+		// return content and share buttons
+		return $htmlContent;
+	}
+
+	// add answer option to the end of the post
+	add_filter( 'the_content', 'show_share_buttons');	
+	
+?>
